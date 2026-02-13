cd85e838fd45203e6141eb32667cfdf92ccde8fe
Christopher W. Schaeffer <adeptcode1@gmail.com>
2026-02-13 06:41:22 -0500

    First Commit

---
+++ Chapter01/Chapter/plants.js
@@ -0,0 +1,16 @@
+/*
+    Javascript 6th Edition
+    Chapter 1
+    Chapter case
+
+    Tinley Xeriscapes
+    Author: Christopher W. Schaeffer
+    Date:   2026-02-12
+    
+    Filename: plants.js
+*/
+
+// define variables containing img src values
+      var blanket = "images/blanket.jpg";
+      var bluestem = "images/bluestem.jpg";
+      var rugosa = "images/rugosa.jpg";
