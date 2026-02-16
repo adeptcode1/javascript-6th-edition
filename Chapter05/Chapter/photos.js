69f50a3d700f74f813d9d62985bf34ab3fc8bb45
Christopher W. Schaeffer <adeptcode1@gmail.com>
2026-02-16 14:20:34 -0500

    Chapter 5 case complete

--- Chapter05/Chapter/photos.js
+++ Chapter05/Chapter/photos.js
@@ -4,8 +4,8 @@
 
  *    Photo gallery
  *    Variables and functions
- *    Author: 
- *    Date:   
+ *    Author:  Christopher W. Schaeffer
+ *    Date:    2026-02-15
 
  *    Filename: photos.js
  */
@@ -14,9 +14,33 @@
 
 /* global variables */
 var photoOrder = [1,2,3,4,5];
+var figureCount = 3;
+var autoAdvance = setInterval(rightAdvance, 5000);
+/* add src value to img elements based on order specified in photoOrder array */
+function populateFigures() {
+   var filename;
+   var currentFig;
+   if (figureCount === 3) {
+      for (var i = 1; i < 4; i++) {
+         filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+         currentFig = document.getElementsByTagName("img")[i - 1];
+         currentFig.src = filename;
+      }
+   } else {
+      for (var i = 0; i < 5; i++) {
+         filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+         currentFig = document.getElementsByTagName("img")[i];
+         currentFig.src = filename;
+      }
+   }
+}
 
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
+   clearInterval(autoAdvance);
+   rightAdvance();
+}
+function rightAdvance() {
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] + 1) === 6) {
          photoOrder[i] = 1;
@@ -29,6 +53,7 @@ function rightArrow() {
 
 /* shift all images one figure to the right, and change values in photoOrder array to match  */
 function leftArrow() {
+   clearInterval(autoAdvance);
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] - 1) === 0) {
          photoOrder[i] = 5;
@@ -39,9 +64,111 @@ function leftArrow() {
    }
 }
 
+/* switch to 5-image layout*/
+function previewFive() {
+   //Create figure and img elements for fifth image
+   var lastFigure = document.createElement("figure");
+   lastFigure.id = "fig5";
+   lastFigure.style.zIndex = "5";
+   lastFigure.style.position = "absolute";
+   lastFigure.style.right ="45px";
+   lastFigure.style.top = "67px";
+
+   var lastImage = document.createElement("img");
+   lastImage.width = "240";
+   lastImage.height = "135";
+
+   var articleE1 = document.getElementsByTagName("article")[0];
+   
+   lastFigure.appendChild(lastImage);
+   //articleE1.appendChild(lastFigure);
+   articleE1.insertBefore(lastFigure, document.getElementById("rightarrow"));
+
+   //Clone figure element for fifth image and edit to be first image
+   var firstFigure = lastFigure.cloneNode(true);
+   firstFigure.id = "fig1";
+   firstFigure.style.right = "";
+   firstFigure.style.left = "45px";
+   //articleE1.appendChild(firstFigure);
+   articleE1.insertBefore(firstFigure, document.getElementById("fig2"));
+
+   //add appropriate src values to two new img elements
+   document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+   document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+   figureCount = 5;
+   //change button to hide extra images
+   var numberButton = document.querySelector("#fiveButton p")
+   numberButton.innerHTML = "Show fewer images";
+   if (numberButton.addEventListener) {
+      numberButton.removeEventListener("click", previewFive, false);
+      numberButton.addEventListener("click", previewThree, false);
+   } else if (numberButton.attachEvent) {
+      numberButton.detachEvent("onclick", previewFive);
+      numberButton.attachEvent("onclick", previewThree);
+   }
+
+}
+
+/* switch to 3-image layout */
+function previewThree() {
+   var articleE1 = document.getElementsByTagName("article")[0];
+   var numberButton = document.querySelector("#fiveButton p");
+   articleE1.removeChild(document.getElementById("fig1"));
+   articleE1.removeChild(document.getElementById("fig5"));
+   figureCount = 3;
+   numberButton.innerHTML = "Show fewer images";
+   if (numberButton.addEventListener) {
+      numberButton.removeEventListener("click", previewThree, false);
+      numberButton.addEventListener("click", previewFive, false);
+   } else if (numberButton.attachEvent) {
+      numberButton.detachEvent("onclick", previewThree);
+      numberButton.attachEvent("onclick", previewFive);
+   }
+}
+/* create event listeners for left arrow, right arrow, and
+   center figure elements
+*/
+function createEventListeners() {
+   var leftarrow = document.getElementById("leftarrow");
+   if (leftarrow.addEventListener){
+      leftarrow.addEventListener("click", leftArrow, false);
+   } else if (leftarrow.attachEvent) {
+      leftarrow.attachEvent("onclick", leftArrow);
+   }
+
+   var rightarrow = document.getElementById("rightarrow");
+   if (rightarrow.addEventListener){
+      rightarrow.addEventListener("click", rightArrow, false);
+   } else if (leftarrow.attachEvent) {
+      rightarrow.attachEvent("onclick", rightArrow);
+   }
+
+   var mainFig = document.getElementsByTagName("img")[1];
+   if (mainFig.addEventListener){
+      mainFig.addEventListener("click", zoomFig, false);
+   } else if (mainFig.attachEvent) {
+      mainFig.attachEvent("onclick", zoomFig);
+   }
+
+   var showAllButton = document.querySelector("#fiveButton p");
+   if (showAllButton.addEventListener) {
+      showAllButton.addEventListener("click", previewFive, false);
+   } else if (showAllButton.attachEvent) {
+      showAllButton.attachEvent("onclick", previewFive);
+
+   }
+}
 /* open center figure in separate window */
 function zoomFig() {
-   
+   var propertyWidth = 960;
+   var propertyHeight = 600;
+   var winLeft = ((screen.width - propertyWidth) / 2);
+   var winTop = ((screen.height - propertyHeight) / 2);
+   var winOptions = "width = 960,height=600";
+   winOptions += ",left=" + winLeft;
+   winOptions += ",top=" + winTop;
+   var zoomWindow = window.open("zoom.htm", "zoomwin", winOptions);
+   zoomWindow.focus();
 }
 
 /* create event listeners and populate image elements */
--- Chapter05/Chapter/zoom.js
+++ Chapter05/Chapter/zoom.js
@@ -4,8 +4,8 @@
 
  *    Photo zoom
  *    Variables and functions
- *    Author: 
- *    Date:   
+ *    Author:  Christopher W. Schaeffer
+ *    Date:    2026-02-16
 
  *    Filename: zoom.js
  */
@@ -19,7 +19,23 @@ var figFilename = "images/IMG_0" + photoOrderArray[2] + ".jpg";
 /* populate img element and create event listener */
 function pageSetup() {
    document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
+   createEventListener();
 }
 
+/* close window */
+function closeWin() {
+   window.close();
+}
+
+/* create event listener for close button */
+function createEventListener() {
+   var closeWindowDiv = document.getElementsByTagName("p")[0];
+   if (closeWindowDiv.addEventListener) {
+      closeWindowDiv.addEventListener("click", closeWin, false);
+   } else if (closeWindowDiv.attachEvent){
+      closeWindowDiv.attachEvent("onclick", closeWin);
+
+   }
+}
 /* add img src value and create event listener when page finishes loading */
 window.onload = pageSetup;
